4a922d293e434125864b1b989c8fba05d44e3916
paulasopena <paulasopenacoello@gmail.com>
2023-01-12 12:22:20 +0100

    Abuse features and chat added service

---
+++ src/main/java/edu/upc/dsa/models/ChatMessage.java
@@ -0,0 +1,28 @@
+package edu.upc.dsa.models;
+
+public class ChatMessage {
+    String name;
+    String message;
+    public ChatMessage(){}
+
+    public ChatMessage(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
